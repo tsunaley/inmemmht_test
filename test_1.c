40ee69c14a5335ecf3b2682c85a79a04a146b1d4
tsunaley <775075658@qq.com>
2022-09-28 11:23:32 +0800

    update 50

--- test_1.c
+++ test_1.c
@@ -43,7 +43,7 @@ int main(int argc, char const *argv[])
 	selectHash(pmhtroot, sql);
 	// sql = "UPDATE gps SET Lng=1511 WHERE Lng=1501";
 	// updateHash(pmhtroot, sql);
-	updateTime(pmhtroot, 10);
+	updateTime(pmhtroot, 50);
 	free_mht_postorder(&pmhtroot);
 }
 
